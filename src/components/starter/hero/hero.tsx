68a0dafd0e77aa984fad43119bf88796868e2b8e
Mario Montero <marioupc3@gmail.com>
2023-03-29 16:22:05 -0500

    adding styles to search

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -6,17 +6,34 @@ interface HeroProps {
 }
 export default component$((props: HeroProps) => {
   return (
-    <div class={styles.hero}>
-      <h1>Traduce tu ingles con AI</h1>
-      <input
-        class={styles.searchInput}
-        type="search"
-        onKeyUp$={(event: any) => {
-          props.translateText(event?.target?.value);
-        }}
-        
-        placeholder="Ingresa tu texto"
-      />
-    </div>
+    <>
+      <div class={styles.hero}>
+        <h1>Traduce tu ingles con AI</h1>
+        <input
+          class={styles.searchInput}
+          type="search"
+          onKeyUp$={(event: any) => {
+            props.translateText(event?.target?.value);
+          }}
+          onClick$={(event: any) => {
+            if (event?.target?.value.length === 0) {
+              props.translateText("");
+            }
+          }}
+          placeholder="Ingresa tu texto"
+        />
+      </div>
+
+      {/* <div class={styles.heroButtons}>
+        <button
+          class={styles.heroButton}
+          onClick$={() => {
+            props.translateText("Hello");
+          }}
+        >
+          Quiero ver magia!
+        </button>
+      </div> */}
+    </>
   );
 });
