33f0975a049a823eb3abce504347244444c3374e
Mario Montero <marioupc3@gmail.com>
2023-04-03 11:48:20 -0500

    linkedin link

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,15 +1,25 @@
-import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
-import styles from './footer.module.css';
+import { component$ } from "@builder.io/qwik";
+import { useServerTimeLoader } from "~/routes/layout";
+import styles from "./footer.module.css";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
-  console.log("serverTime.value",serverTime.value)
+  console.log("serverTime.value", serverTime.value);
 
-  console.log(":::Asd")
+  console.log(":::Asd");
   return (
     <footer>
-      <a href="https://github.com/MarioGalvan" target="_blank" class={styles.anchor}>
+      <a
+        href="https://co.linkedin.com/in/mdmontero?trk=profile-badge"
+        target="_blank"
+        class={styles.anchor}
+      >
+        <img
+          src="https://play-lh.googleusercontent.com/kMofEFLjobZy_bCuaiDogzBcUT-dz3BBbOrIEjJ-hqOabjK8ieuevGe6wlTD15QzOqw"
+          alt="Logo"
+          class={styles.logo}
+          width={50}
+        />
         Hecho por Mario Montero 😎
         {/* <span class={styles.spacer}>|</span> */}
         {/* <span>{serverTime.value.date.toLocaleLowerCase()}</span> */}
